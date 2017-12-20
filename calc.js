4aa1fffa1f98f9238723103135e039c86b314c66
Maggie Hubbard <maggiehubbard@wayne.edu>
2017-12-19 19:52:45 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,33 @@
+
+//declare valiables
+
+
+
+function toBox (x){
+    var box = document.getElementById("display");
+    box.value += x;
+    if (x==='C'){
+        box.value='';
+    }
+} 
+
+function answer(){
+    var box = document.getElementById("display");
+    x=box.value
+    x=eval(x);
+    box.value=x;
+}
+function sqRt()
+{
+var box = document.getElementById("display");
+x=box.value;
+x=eval(x*x);
+box.value=x;
+}
+function backSpace(){
+    var box = document.getElementById("display");
+    var num = box.value;
+    var len = num.length-1;
+    var newNumber = num.substring(0, len);
+    box.value = newNumber;   
+}
